6d4009bccbf840afd591e9f3fda172729739b5a5
MorineWanjiru440 <waweru120929@students.must.ac.ke>
2025-10-05 14:33:33 +0300

    Design Circular Queue

---
+++ circular queue.c
@@ -0,0 +1,13 @@
+typedef struct {
+    int *data;
+    int front, rear, size, capacity;
+} MyCircularQueue;
+
+MyCircularQueue* myCircularQueueCreate(int k);
+bool myCircularQueueEnQueue(MyCircularQueue* obj, int value);
+bool myCircularQueueDeQueue(MyCircularQueue* obj);
+int myCircularQueueFront(MyCircularQueue* obj);
+int myCircularQueueRear(MyCircularQueue* obj);
+bool myCircularQueueIsEmpty(MyCircularQueue* obj);
+bool myCircularQueueIsFull(MyCircularQueue* obj);
+void myCircularQueueFree(MyCircularQueue* obj);
